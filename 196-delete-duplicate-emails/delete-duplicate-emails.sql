997430030c3d21cf479c3795b1adb1cc70040c25
Tianya Chen <tianyakc@gmail.com>
2025-05-11 12:47:05 -0400

    Time: 935 ms (37.30%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,7 +1,11 @@
-with cte as (
-    select id, email, row_number() over(partition by email order by id) rn
-    from Person
-)
-delete 
-from Person
-where id in (select id from cte where rn > 1)
+-- with cte as (
+--     select id, email, row_number() over(partition by email order by id) rn
+--     from Person
+-- )
+-- delete 
+-- from Person
+-- where id in (select id from cte where rn > 1)
+
+delete p1
+from Person p1 join Person p2
+on p1.email = p2.email and p1.id > p2.id;

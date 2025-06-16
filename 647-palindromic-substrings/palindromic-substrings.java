2f307c4e02402e3c41ad215bceab5bafd3b03b37
Tianya Chen <tianyakc@gmail.com>
2025-06-16 12:09:11 -0400

    Time: 5 ms (97.16%) | Memory: 41.9 MB (39.62%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,22 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
         int count = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                if (isPalindrome(s, i, j)) {
-                    count++;
-                }
-            }
+            count += countPalindromes(s, i, i);
+            count += countPalindromes(s, i, i+1);
         }
+
         return count;
     }
 
-    private boolean isPalindrome(String s, int begin, int end) {
-        while (begin < end) {
-            if (s.charAt(begin++) != s.charAt(end--)) {
-                return false;
-            }
+    private int countPalindromes(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
         }
-        return true;
+        return count;
     }
 }

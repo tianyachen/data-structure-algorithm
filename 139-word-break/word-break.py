8fec35d2bf2a1b236e3b46383ff33f8ba8651a79
Tianya Chen <tianyakc@gmail.com>
2025-06-18 11:47:26 -0400

    Time: 3 ms (71.09%) | Memory: 18.5 MB (6.53%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        root = TrieNode()
+        for word in wordDict:
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.is_word = True
+
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            if i == 0 or dp[i - 1]:
+                curr = root
+                for j in range(i, len(s)):
+                    c = s[j]
+                    if c not in curr.children:
+                        break
+                    
+                    curr = curr.children[c]
+                    if curr.is_word:
+                        dp[j] = True
+        
+        return dp[-1]
+                

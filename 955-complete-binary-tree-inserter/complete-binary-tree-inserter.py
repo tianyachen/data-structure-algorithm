759adea002a7a54d3e4fed6c204508ddfcc1aecb
Tianya Chen <tianyakc@gmail.com>
2025-08-15 16:14:40 -0400

    Time: 1 ms (69.76%) | Memory: 18.7 MB (66.32%) - LeetSync

---
+++ 955-complete-binary-tree-inserter/complete-binary-tree-inserter.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class CBTInserter:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.queue = deque()
+
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+
+            if not node.left or not node.right:
+                self.queue.append(node)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+    def insert(self, val: int) -> int:
+        parent = self.queue[0]
+        newNode = TreeNode(val)
+
+        if not parent.left:
+            parent.left = newNode
+        elif not parent.right:
+            parent.right = newNode
+            self.queue.popleft()
+        
+        self.queue.append(newNode)
+        return parent.val
+
+    def get_root(self) -> Optional[TreeNode]:
+        return self.root
+
+
+# Your CBTInserter object will be instantiated and called as such:
+# obj = CBTInserter(root)
+# param_1 = obj.insert(val)
+# param_2 = obj.get_root()
